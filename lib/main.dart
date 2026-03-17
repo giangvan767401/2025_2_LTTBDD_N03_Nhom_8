f84b5e637e54eb67ae6e6e1784cad081ac4f94e8
HuuTinhne <joshuutinh205@gmail.com>
2026-03-17 16:26:19 +0700

    ket noi provider vao he thong

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'quan_ly_ngon_ngu.dart';
 import 'man_hinh/man_hinh_dong_ho.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => QuanLyNgonNgu(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
