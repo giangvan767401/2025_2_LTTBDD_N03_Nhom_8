37cc9d82381fae455c766d636e4f26159fb35e7a
HuuTinhne <joshuutinh205@gmail.com>
2026-03-17 15:30:40 +0700

    Add basic group info screen

---
+++ lib/man_hinh/man_hinh_thong_tin_nhom.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ManHinhThongTinNhom extends StatelessWidget {
+  const ManHinhThongTinNhom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Thông tin nhóm'),
+      ),
+      body: const Center(
+        child: Text('Trang Thông tin nhóm (Đang xây dựng)'),
+      ),
+    );
+  }
+}
